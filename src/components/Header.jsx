7641782a7f71a5df89c335d3ed83c6df88c7e88d
Manpreet <manpreet.null@agilitys-imac.uleth.ca>
2026-06-24 19:12:52 -0600

    inital commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import "../css/Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        ResumePro
+      </div>
+
+      <nav className="nav">
+        <a href="#features">Features</a>
+        <a href="#templates">Templates</a>
+        <a href="#pricing">Pricing</a>
+        <a href="#faq">FAQ</a>
+      </nav>
+
+      <button className="login-btn">
+        Login
+      </button>
+    </header>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import "../css/Hero.css"
+
+export default function Hero(){
+    return(
+       
+            <section className="hero">
+                <div className="hero-content">
+                    <h1>Create a Professional Resume in Minutes</h1>
+                    <p>
+                        Build your resume with modern templates and 
+                        download it instantly. 
+                    </p>
+                    <button>Create Resume</button>
+                </div>
+                <div className="hero-image">
+                    <img src="../assets/hero-banner-res-examples-desktop.png" alt="Resume Preview " 
+                    />
+                    </div>
+            </section>
+
+            
+    
+
+    );
+}
